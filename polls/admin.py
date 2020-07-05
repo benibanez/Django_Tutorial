77225d7e03aa2fbf95a19591f74185ef0ce42986
Benjamin Inman <binman@MacBook-Pro.local>
2020-07-05 00:18:10 -0400

    Django Tutorial Complete

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
 # Register your models here.
-admin.site.register(Question)
+#admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}), #the 'Date information' sets the title for the field set in the blue box in the admin form
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
